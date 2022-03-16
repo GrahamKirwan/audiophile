33da78b2b6774c5f566739c2801b2bafd8bb7a01
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-15 22:34:19 -0400

    card components animations

--- components/Reusable/ThreeCardProducts.js
+++ components/Reusable/ThreeCardProducts.js
@@ -3,24 +3,37 @@ import React from "react";
 import styles from "../Reusable/ThreeCardProducts.module.scss";
 
 export default function ThreeCardProducts(props) {
-    let isFirstComponent = props.first ? styles.largeMargin : '';
+  let isFirstComponent = props.first ? styles.largeMargin : "";
 
   return (
     <div className={`${styles.cardsContainer} ${isFirstComponent}`}>
-        <a href="#" className={styles.card}>
-            <img src="/images/shared/headphones.png"></img>
-            <img className={styles.shadow} src="/images/shared/shadow.png"></img>
-            <h4>Headphones</h4>
-            <h5>Shop</h5>
-        </a>
-        <a href="#" className={styles.card}>
-            <h4>Speakers</h4>
-            <h5>Shop</h5>
-        </a>
-        <a href="#" className={styles.card}>
-            <h4>Earphones</h4>
-            <h5>Shop</h5>
-        </a>
+      <a href="#" className={styles.card}>
+        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/headphones.png"></img>
+        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+        <h4>Headphones</h4>
+        <div className={styles.shop}>
+          <h5>Shop</h5>
+          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+        </div>
+      </a>
+      <a href="#" className={styles.card}>
+        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/speakers.png"></img>
+        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+        <h4>Speakers</h4>
+        <div className={styles.shop}>
+          <h5>Shop</h5>
+          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+        </div>
+      </a>
+      <a href="#" className={styles.card}>
+        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/earphones.png"></img>
+        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+        <h4>Earphones</h4>
+        <div className={styles.shop}>
+          <h5>Shop</h5>
+          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+        </div>
+      </a>
     </div>
-  )
+  );
 }
