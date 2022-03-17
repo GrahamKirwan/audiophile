379aa3b1808e0472b4e5d8f56f92efa652d92684
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-17 16:59:59 -0400

    grey speaker component

---
+++ components/Reusable/GreySpeakerFeature.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from '../Reusable/GreySpeakerFeature.module.scss'
+
+export default function GreySpeakerFeature() {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.scss'
 import HeroSection from '../components/Home/HeroSection'
 import ThreeCardProducts from '../components/Reusable/ThreeCardProducts'
 import OrangeSpeakerFeature from '../components/Reusable/OrangeSpeakerFeature'
+import GreySpeakerFeature from '../components/Reusable/GreySpeakerFeature'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <HeroSection />
       <ThreeCardProducts first={true} />
       <OrangeSpeakerFeature />
+      <GreySpeakerFeature />
     </>
   )
 }
