23b1dbe64bab3c05a15b94fd54f1360be434242d
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-18 16:00:26 -0400

    grey speaker component

--- components/Reusable/GreySpeakerFeature.js
+++ components/Reusable/GreySpeakerFeature.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import styles from '../Reusable/GreySpeakerFeature.module.scss'
+import btnStyles from "../../styles/UI/ButtonStyles.module.scss";
 
 export default function GreySpeakerFeature() {
   return (
     <div className={styles.container}>
-        
+        <h3>ZX7 SPEAKER</h3>
+        <a href='#' className={btnStyles.btnThree}>See product</a>
     </div>
   )
 }
