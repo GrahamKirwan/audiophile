4693d9170ee6461cdfad068cc4cdc1af8c2872ae
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-24 21:03:34 -0400

    fixed three card links

--- components/Reusable/ThreeCardProducts.js
+++ components/Reusable/ThreeCardProducts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "../Reusable/ThreeCardProducts.module.scss";
 
@@ -7,33 +8,39 @@ export default function ThreeCardProducts(props) {
 
   return (
     <div className={`${styles.cardsContainer} ${isFirstComponent}`}>
-      <a href="#" className={styles.card}>
-        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/headphones.png"></img>
-        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
-        <h4>Headphones</h4>
-        <div className={styles.shop}>
-          <h5>Shop</h5>
-          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
-        </div>
-      </a>
-      <a href="#" className={styles.card}>
-        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/speakers.png"></img>
-        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
-        <h4>Speakers</h4>
-        <div className={styles.shop}>
-          <h5>Shop</h5>
-          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
-        </div>
-      </a>
-      <a href="#" className={styles.card}>
-        <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/earphones.png"></img>
-        <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
-        <h4>Earphones</h4>
-        <div className={styles.shop}>
-          <h5>Shop</h5>
-          <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
-        </div>
-      </a>
+      <Link href="/headphones">
+        <a className={styles.card}>
+            <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/headphones.png"></img>
+            <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+            <h4>Headphones</h4>
+            <div className={styles.shop}>
+            <h5>Shop</h5>
+            <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+            </div>
+        </a>
+      </Link>
+      <Link href="/speakers">
+        <a className={styles.card}>
+            <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/speakers.png"></img>
+            <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+            <h4>Speakers</h4>
+            <div className={styles.shop}>
+            <h5>Shop</h5>
+            <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+            </div>
+        </a>
+      </Link>
+      <Link href="/earphones">
+        <a className={styles.card}>
+            <img className={`${styles.cardImage} ${styles.cardImageMain}`} src="/images/shared/earphones.png"></img>
+            <img className={`${styles.shadow} ${styles.cardImage}`} src="/images/shared/shadow.png"></img>
+            <h4>Earphones</h4>
+            <div className={styles.shop}>
+            <h5>Shop</h5>
+            <img src="/images/shared/desktop/icon-arrow-right.svg"></img>
+            </div>
+        </a>
+      </Link>
     </div>
   );
 }
