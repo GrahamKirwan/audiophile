14f1fcc1e904e665a228b7e0e092e1afca2ffe48
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-26 10:23:09 -0400

    data mapping

--- pages/headphones/index.js
+++ pages/headphones/index.js
@@ -4,10 +4,23 @@ import HeroMain from '../../components/Reusable/HeroMain'
 import ThreeCardProducts from '../../components/Reusable/ThreeCardProducts'
 import AboutComp from '../../components/Reusable/AboutComp'
 
+import data from '../../data/data.json'
+
 export default function HeadphonesPage() {
+
+    const newData = [];
+    
+    data.map((data) => {
+        if (data.category == 'headphones') {
+            newData.push(data);
+        }
+    })
+
   return (
     <>
         <HeroMain page='Headphones' />
+        {newData.map((product) => <h1>{product.name}</h1>)}
+        {newData.map((product) => <h1>{product.name}</h1>)}
         <ThreeCardProducts />
         <AboutComp />
     </>
