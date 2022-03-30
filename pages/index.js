f32c0f47802d87de4403e5da1f46d8db07241897
Graham Kirwan <graham.kirwan4@gmail.com>
2022-03-29 22:08:10 -0400

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-
 import HeroSection from '../components/Home/HeroSection'
 import ThreeCardProducts from '../components/Reusable/ThreeCardProducts'
 import OrangeSpeakerFeature from '../components/Reusable/OrangeSpeakerFeature'
