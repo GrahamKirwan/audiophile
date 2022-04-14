24a65df44e00db1fe5873d3a1a112971ee7ccf73
Graham Kirwan <graham.kirwan4@gmail.com>
2022-04-13 21:41:06 -0400

    cart updates

--- components/layout/Overlay.js
+++ components/layout/Overlay.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 
-import styles from '../layout/Overlay.module.scss'
+import styles from "../layout/Overlay.module.scss";
+import btnStyles from "../../styles/UI/ButtonStyles.module.scss";
 
 export default function Overlay(props) {
+  // Pause scrolling on body when overlay is showing
+  useEffect(() => {
+    if (props.show) {
+      document.body.style.overflow = "hidden";
+    } else if (props.show == false) {
+      document.body.style.overflow = "unset";
+    }
+  }, [props.show]);
 
-    // Pause scrolling on body when overlay is showing
-    useEffect(() => {
-        if(props.show) {
-            document.body.style.overflow = 'hidden';
-        } else if (props.show == false) {
-            document.body.style.overflow = 'unset';
-        }
-     }, [props.show]);
-
-     function overlayClickHandler(e) {
-         if (e.target.tagName == 'DIV')
-         {
-             console.log('clicked on div')
-            props.overlayClickHandler();
-         }
-     }
+  function overlayClickHandler(e) {
+    if (e.target.className.substring(0, 15) == "Overlay_overlay") {
+      props.overlayClickHandler();
+    }
+  }
 
   return (
-    <div onClick={overlayClickHandler} className={styles.overlay} style={props.show ? {display: 'initial'} : {display: 'none'}}>
-        <div className={styles.cartContainer}>
-            <aside className={styles.cart}>CART</aside>
-        </div>
+    <div
+      onClick={overlayClickHandler}
+      className={styles.overlay}
+      style={props.show ? { display: "initial" } : { display: "none" }}
+    >
+      <div className={styles.cartContainer}>
+        <aside className={styles.cart}>
+          <div className={styles.cartContentContainer}>
+            <div className={styles.cartContentContainer_top}>
+                <h3>CART (4)</h3>
+                <p>Remove all</p>
+            </div>
+            <div className={styles.cartContentContainer_middle}>
+                <p>test</p>
+            </div>
+            <div className={styles.cartContentContainer_bottom}>
+                <h5>Total</h5>
+                <p>$21,345</p>
+            </div>
+            <a style={{width: '100%'}} className={btnStyles.btnPrimary}>See product</a>
+          </div>
+        </aside>
+      </div>
     </div>
-  )
+  );
 }
